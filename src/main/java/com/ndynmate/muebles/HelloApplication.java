8690754e900f71538563a5e8f1fdabc86d59483b
czujko <55107723+czujko@users.noreply.github.com>
2021-12-16 20:22:17 +0100

    app icon added

--- src/main/java/com/ndynmate/muebles/HelloApplication.java
+++ src/main/java/com/ndynmate/muebles/HelloApplication.java
@@ -3,6 +3,7 @@ package com.ndynmate.muebles;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mueble-vista.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/icono/icono.png")));
         stage.setTitle("Muebles v.1");
         stage.setScene(scene);
         stage.show();
